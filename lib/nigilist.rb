09637806fceca9706cf9123b805e21b3a1fedc3c
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-05 15:05:03 +0100

    implements return of [] for plural methods

--- lib/nigilist.rb
+++ lib/nigilist.rb
@@ -3,6 +3,8 @@ module Nigilist
     method_name = message.to_s
     if method_name.end_with?('?')
       false
+    elsif method_name.split("_").last.end_with?('s')
+      []
     elsif !method_name.end_with?('!')
       nil
     else
