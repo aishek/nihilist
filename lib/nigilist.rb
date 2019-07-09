8119c9a51fb789c3c5deec12b57f496f7572e462
Alexandr Borisov <ab@cifronomika.ru>
2019-07-09 23:29:20 +0300

    implement ActiveRecord null relation return

--- lib/nigilist.rb
+++ lib/nigilist.rb
@@ -1,14 +1,31 @@
+# frozen_string_literal: true
+
 module Nigilist
   def method_missing(message, *args, &block)
     method_name = message.to_s
-    if method_name.end_with?('?')
-      false
-    elsif method_name.split("_").last.end_with?('s')
-      []
-    elsif !method_name.end_with?('!')
-      nil
-    else
-      super
+
+    return false if method_name.end_with?('?')
+
+    if method_name.end_with?('s') && method_name[-2..-1] != 'ss'
+      if defined?(Rails)
+        ar_relation_klass = method_name.singularize.camelize.safe_constantize
+        return ar_relation_klass.none if ar_relation_klass && ar_relation_klass.respond_to?(:none)
+      end
+
+      return []
     end
+
+    return nil unless method_name.end_with?('!')
+
+    super
+  end
+
+  def respond_to_missing?(message, include_private = false)
+    method_name = message.to_s
+
+    method_name.end_with?('?') ||
+    (method_name.end_with?('s') && method_name[-2..-1] != 'ss') ||
+    !method_name.end_with?('!') ||
+      super
   end
 end
